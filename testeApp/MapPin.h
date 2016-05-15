f15bb7153923fb7281d170159f11e67558f9b40f
students@deti <deti@JMFs-Mac-mini.local>
2016-05-15 23:53:36 +0100

    current location and map

---
+++ testeApp/MapPin.h
@@ -0,0 +1,13 @@
+//
+//  MapPin.h
+//  testeApp
+//
+//  Created by students@deti on 15/05/16.
+//  Copyright © 2016 students@deti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapPin : NSObject
+
+@end
---
+++ testeApp/MapPin.m
@@ -0,0 +1,13 @@
+//
+//  MapPin.m
+//  testeApp
+//
+//  Created by students@deti on 15/05/16.
+//  Copyright © 2016 students@deti. All rights reserved.
+//
+
+#import "MapPin.h"
+
+@implementation MapPin
+
+@end
